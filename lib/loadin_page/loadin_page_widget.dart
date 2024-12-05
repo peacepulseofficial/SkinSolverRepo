136afd74a07bf459c84d57c99a43cf455d6add8b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-12-05 19:14:17 +0000

    Updating to latest FlutterFlow output.

--- lib/loadin_page/loadin_page_widget.dart
+++ lib/loadin_page/loadin_page_widget.dart
@@ -2,6 +2,7 @@ import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/actions/index.dart' as actions;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'loadin_page_model.dart';
@@ -32,6 +33,8 @@ class _LoadinPageWidgetState extends State<LoadinPageWidget> {
       } else {
         context.goNamed('WelcomeToAppPage');
       }
+
+      await actions.appTracking();
     });
   }
 
